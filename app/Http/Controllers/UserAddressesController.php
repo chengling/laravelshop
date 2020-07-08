021ab2f7dc9f18dc047b3c92baed9651cce028d3
cl1688 <1245010531@qq.com>
2020-07-08 18:01:51 +0800

    新增收货地址

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -12,4 +12,25 @@ class UserAddressesController extends Controller
             'addresses' => $request->user()->addresses,
         ]);
     }
+    
+    public function create()
+    {
+    	return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
+    }
+    
+    
+    public function store(UserAddressRequest $request)
+    {
+    	$request->user()->addresses()->create($request->only([
+    			'province',
+    			'city',
+    			'district',
+    			'address',
+    			'zip',
+    			'contact_name',
+    			'contact_phone',
+    			]));
+    
+    	return redirect()->route('user_addresses.index');
+    }
 }
