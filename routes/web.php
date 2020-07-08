8973d9b3cdde1f765bf0b208c75abe321cc5dcc1
cl1688 <1245010531@qq.com>
2020-07-08 17:53:01 +0800

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PagesController@root')->name('root');
+Auth::routes(['verify' => true]);
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth', 'verified']], function() {
+	Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
