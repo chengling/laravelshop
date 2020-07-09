3d7720068e1e8a43130bdd8271d2124a3e92336f
cl1688 <1245010531@qq.com>
2020-07-09 13:54:49 +0800

    异常处理

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -55,8 +55,8 @@ class ProductsController extends Controller
    {
    	// 判断商品是否已经上架，如果没有上架则抛出异常。
    	if (!$product->on_sale) {
-   		throw new \Exception('商品未上架');
-   	}
+            throw new InvalidRequestException('商品未上架');
+   		   	}
    
    	return view('products.show', ['product' => $product]);
    }
