01add829ceaba62608f851d50c8822e6e3e6de5f
cl1688 <1245010531@qq.com>
2020-07-11 11:54:57 +0800

    添加同步命令

--- config/database.php
+++ config/database.php
@@ -145,7 +145,7 @@ return [
     ],
     'elasticsearch' => [
     // Elasticsearch 支持多台服务器负载均衡，因此这里是一个数组
-    'hosts' => explode(',', env('ES_HOSTS')),
+    	'hosts' => explode(',', env('ES_HOSTS')),
     ]
 
 ];
